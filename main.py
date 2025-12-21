6d6c7354dc318e326d688686c84e82f4b134620e
Oksana Polulyakh <o.a.polulyakh@gmail.com>
2025-12-21 19:22:10 +0300

    Add users in DB

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import telebot
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+import sqlalchemy as sq
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import User, Word, create_tables
+
+load_dotenv()
 token = os.getenv('token')
+DSN = os.getenv('DSN')
+
+engine = create_engine(DSN)
 bot = telebot.TeleBot(token)
+create_tables(engine)
+Session = sessionmaker(bind=engine)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет, со мной ты можешь выучить английские слова, Давай приступим!")
+def start(message):
+    cid = message.chat.id
+    username = message.from_user.username
+    with Session() as session:
+        user = session.query(User).filter(User.cid == cid).first()
+        if user:
+            bot.send_message(chat_id=cid, text=f"Привет! Приятно тебя снова видеть здесь)")
+        else:
+            try:
+                obj = User(cid=cid, username=username)
+                session.add(obj)
+                session.commit()
+                bot.send_message(chat_id=cid, text=f"Привет, приятно познакомиться")
+            except Exception as e:
+                print(e)
+                session.rollback()
+
+if "__main__" == __name__:
+    print("Bot working...")
+    bot.polling()
--- models.py
+++ models.py
@@ -1,15 +1,20 @@
 import sqlalchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    CID = Column(Integer, unique=True, nullable=False)
-    username = Column(String, unique=True, nullable=False)
+    cid = Column(BigInteger, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=True)
 
-class Words(Base):
+    words = relationship("Word", back_populates="user", cascade="all, delete-orphan")
+
+    def __str__(self):
+        return f"User {self.CID}: {self.username}"
+
+class Word(Base):
     __tablename__ = 'words'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
@@ -17,3 +22,10 @@ class Words(Base):
     translation = Column(String, nullable=False)
 
     user = relationship("User", back_populates="words")
+
+    def __str__(self):
+        return f"Word {self.word} - > {self.translation}"
+
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
